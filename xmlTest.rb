5672ca3be0adf2db0bfaf1ef36f5808825665c92
PillGrimDestroyer <megamega4@yandex.ru>
2019-10-14 00:17:39 +0600

    Delete xmlTest.rb

--- xmlTest.rb
+++
@@ -1,9 +0,0 @@
-str = "I will drill for a well in Hull."
-regex = Regexp.new(/\w+ll/)
-
-matchdata = regex.match(str)
-while matchdata != nil
-  puts matchdata[0]
-  str = matchdata.post_match
-  matchdata = regex.match(str)
-end
